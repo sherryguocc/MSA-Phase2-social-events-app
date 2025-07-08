1927f1998992a0ec6273ca1e310fbeafd209e2c5
sherryguocc <sherryguocc@gmail.com>
2025-07-08 19:59:14 +1200

    install daisyui in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import './index.css';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind CSS is working! 🎉
+    <div className="h-screen w-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind + DaisyUI
       </h1>
+      <button className="btn w-64 rounded-full">Button</button>
     </div>
   );
 }
