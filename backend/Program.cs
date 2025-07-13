380097179d1c46c991fc246b8464bb9f67503e32
Sherry Guo <126029204+sherryguocc@users.noreply.github.com>
2025-07-14 10:44:56 +1200

    feat: add EnableRetryOnFailure for SQL resiliency

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,7 +9,15 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    sqlServerOptions =>
+        {
+            sqlServerOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(10),
+                errorNumbersToAdd: null
+            );
+        }));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
