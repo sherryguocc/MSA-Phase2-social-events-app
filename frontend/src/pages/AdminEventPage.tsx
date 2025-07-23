a7380937c5ee481701e5e19c3ff2a315360ba744
Sherry Guo <126029204+sherryguocc@users.noreply.github.com>
2025-07-23 17:55:35 +1200

    update manage event page button color

--- frontend/src/pages/AdminEventPage.tsx
+++ frontend/src/pages/AdminEventPage.tsx
@@ -92,7 +92,7 @@ export default function AdminEventPage() {
             </div>
             <button
               onClick={() => handleDelete(event.id)}
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+              className="!bg-red-600 !text-white !font-bold px-5 py-2 rounded-lg shadow-md hover:!bg-red-700"
             >
               Delete
             </button>
